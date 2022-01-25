3eaff44985e30004e71e88a2da4ee3a6693fe507
v-devi.ssihombing <v-devi.ssihombing@L105010008135.bukitmakmur.corp>
2022-01-25 14:27:20 +0700

    add new test case and test suite

---
+++ Test Suites/testtt02/testSuite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4d46ede4-f216-484f-ae92-702bf9d2d56e</testSuiteGuid>
+   <testCaseLink>
+      <guid>26f03d99-abb8-4b3b-befe-e30b6c32b21d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
